6bd460e900f408040a34d4754d971c9472a18957
ryrails <ryrails@suremail.info>
2011-10-05 23:22:04 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '915fb12b4f62ba564c6429d6f523b53dd78011b87df5c8faf117e51d9d99e2478595116f295e575451e4b1b80ea6ea5f491a279835219a088d1de0efb5530306'
